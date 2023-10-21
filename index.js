5c5e6c2443a6e6519a9ce312410a156958720bc4
Thilini Dharmawardhana <dhanu.thilini@gmail.com>
2023-10-21 00:47:08 -0500

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//places index page
+app.use('/places', require('./controllers/places'))
+
+//Home page
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+res.send('Hello world!')
 })
 
+//404 page
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.status(404).send(`<h1>404 Page</h1>`)
 })
 
+
+// Listen to a port number defined by a local environment variable
 //app.listen(3000)
 app.listen(process.env.PORT)
+
+
+
 
