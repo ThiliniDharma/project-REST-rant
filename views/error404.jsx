6f84b9856472198c9fdf8a90b50096e2178f2d1e
Thilini Dharmawardhana <dhanu.thilini@gmail.com>
2023-10-26 01:30:30 -0500

    Add an image to the 404 page and modify style.css

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,9 @@ function error404 () {
     return (
       <Def>
           <main>
+        <div>
+            <img src="/images/cat.jpg" alt="funny cat picture"/>
+        </div> 
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
           </main>
