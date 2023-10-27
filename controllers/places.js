7de950008b0736cccfbff4f3811495888515c6ed
Thilini Dharmawardhana <dhanu.thilini@gmail.com>
2023-10-26 19:31:55 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,12 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
+
+// More code ...
+
 
 router.get('/', (req, res) => {
     let places = [{
@@ -22,10 +30,23 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
-router.post('/', (req, res) => {
+  router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = '/images/cat.jpg'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    //res.send('POST /places')
+    res.redirect('/places')
   })
+  
   
 
 module.exports = router
