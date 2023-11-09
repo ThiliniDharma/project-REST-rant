8fcda754a7a2dfb4489086a629a1996b6be32ad3
Thilini Dharmawardhana <dhanu.thilini@gmail.com>
2023-11-09 01:39:49 -0600

    add a code to Calculate place ratings

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,7 +8,21 @@ function show (data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = sumRatings / data.place.comments.length
+    rating = (
+      <h3>
+        {Math.round(averageRating)} stars
+      </h3>
+    )
     comments = data.place.comments.map(c => {
       return (
         <div className="border">
@@ -38,6 +52,7 @@ function show (data) {
               <div className="col-sm-6">
               <h1>{ data.place.name }</h1>
                 <h2>Rating</h2>
+                {rating}
                 <h5>Not rated</h5>
                 <br></br>
                 <h2>
