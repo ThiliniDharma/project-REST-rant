5dda011aa012cf8c2d17b0d0daaaca260f7cceac
Thilini Dharmawardhana <dhanu.thilini@gmail.com>
2023-11-09 01:45:05 -0600

    create a code to display stars for ratings

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -14,15 +14,19 @@ function show (data) {
     </h3>
   )
   if (data.place.comments.length) {
-    let sumRatings = data.place.comments.reduce((tot, c) => {
-      return tot + c.stars
-    }, 0)
-    let averageRating = sumRatings / data.place.comments.length
-    rating = (
-      <h3>
-        {Math.round(averageRating)} stars
-      </h3>
-    )
+          let sumRatings = data.place.comments.reduce((tot, c) => {
+        return tot + c.stars
+      }, 0)
+      let averageRating = Math.round(sumRatings / data.place.comments.length)
+      let stars = ''
+      for (let i = 0; i < averageRating; i++) {
+        stars += '⭐️'
+      }
+      rating = (
+        <h3>
+          {stars} stars
+        </h3>
+      )
     comments = data.place.comments.map(c => {
       return (
         <div className="border">
@@ -53,7 +57,7 @@ function show (data) {
               <h1>{ data.place.name }</h1>
                 <h2>Rating</h2>
                 {rating}
-                <h5>Not rated</h5>
+                {/* <h5>Not rated</h5> */}
                 <br></br>
                 <h2>
                     Description
