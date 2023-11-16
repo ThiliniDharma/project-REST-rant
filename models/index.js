e33cdb0e41ab1347078146fcebf18dc5628f82f6
Thilini Dharmawardhana <dhanu.thilini@gmail.com>
2023-11-16 00:03:36 -0600

    Modify MONGO_URI

--- models/index.js
+++ models/index.js
@@ -5,6 +5,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 })
+.then(() => console.log('connected to mongo:',process.env.MONGO_URI))
 
 module.exports.Place = require('./places')
 module.exports.Comment = require('./comment')
